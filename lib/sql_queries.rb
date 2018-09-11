617a5ff9c58a26086ba3206f94c7f7dd0ef63bcd
Laura Messa Travin <laura.travin@gmail.com>
2018-09-11 20:43:54 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
  INNER JOIN pledges as b
  ON   a.id = b.user_id
  GROUP BY a.name
- ORDER BY b.amount"
+ ORDER BY a.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
