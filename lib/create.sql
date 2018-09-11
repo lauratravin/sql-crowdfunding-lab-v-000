17f6a825c866c1d2c784f9a52771c36c8176e721
Laura Messa Travin <laura.travin@gmail.com>
2018-09-11 19:41:47 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,11 @@
 CREATE TABLE users (id INTEGER PRIMARY KEY,
                     name TEXT,
                     age INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY,
+                       title TEXT,
+                       category TEXT,
+                       funding_goal TEXT,
+                       start_date DATETIME,
+                       end_date DATETIME)
+
+CREATE TABLE  pledges
