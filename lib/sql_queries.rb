6f2acc9a514fd885d3560c78f106e0adb160b1fb
Laura Messa Travin <laura.travin@gmail.com>
2018-09-11 20:13:36 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 FROM projects AS a
 INNER JOIN  pledges AS b
 ON a.id = b.project_id
-GROUP BY a.title ASC"
+GROUP BY a.title ASC;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
